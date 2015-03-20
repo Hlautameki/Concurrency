19dd0a790bb12cf6aed67070260138a8aa389c19
Mateusz Mistecki <mateusz.mistecki@gmail.com>
2015-03-20 19:20:28 +0100

    Lista wyboru listingu.

--- Chapter2/Program.cs
+++ Chapter2/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace Chapter2
 {
     class Program
     {
         static int _listeningNumber;
+
+        private const string KodNiezrownoleglony = "Kod niezrównoleglony";
+        private const string TworzenieIUruchamianieWatku = "Tworzenie i Uruchamianie wątku";
+        private const string PrzerywanieDzialaniaWatku = "Przerywanie działania wątku";
+        private const string WstrzymywanieIWznawianieDzialaniaWatku = "Wstrzymywanie i wznawianie działania wątku";
+        private const string WatkiDzialajaceWTle = "Wątki działające w tle";
+        private const string ZmianaPriorytetuWatku = "Zmiana priorytetu wątku";
+        private const string ProblemyZGenerowaniemLiczbPseudolosowych = "Problemy z generowaniem liczb pseudolosowych";
+        private const string OsobnyGeneratorLiczbLosowychNaWatek = "Osobny generator liczba losowych na wątek";
+        private const string CzekanieNaUkonczeniePracyWatkuMetodaJoin = "Czekanie na ukończenie pracy wątku metoda join";
+        private const string SekcjeKrytyczne = "Sekcje krytyczne";
+        private const string PrzesylanieDanychDoWatku = "Przesyłanie danych do wątku";
+        private const string PulaWatkow = "Pula wątków";
+        private const string SygnalizacjaZakonczeniaPracyWatku = "Sygnalizacja zakończenia pracy wątku";
+        private const string OperacjeAtomowe = "Operacje atomowe";
+        private const string SystemThreadingTimers = "System.Threading.Timers";
+        private const string SystemTimersTimer = "System.Timers.Timer";
+        private const string Abort = "Abort";
+        private const string Mask = "{0}: {1}";
+        private static bool _exit = false;
 
         static void Main(string[] args)
         {
@@ -14,15 +33,55 @@ namespace Chapter2
             //pi.UseShellExecute = false;
             //Console.WriteLine("Press Enter after attach succeeded");
             //Start.Start(pi);
-            
-            GetListeningNumber();
-            Console.Clear();
 
-            ListingBase listing;
+            while (!_exit)
+            {
+                Console.Clear();
+                GetListeningNumber();
+                Console.Clear();
+                if (!_exit)
+                    TryStart();
+            }
+        }
+
+        private static void GetListeningNumber()
+        {
+            ShowListeningsList();
+            Console.Write("Podaj nr listeningu: ");
+            var line = Console.ReadLine();
+            if (!int.TryParse(line, out _listeningNumber))
+                _exit = true;
+        }
+
+        private static void ShowListeningsList()
+        {
+            Console.WriteLine(Mask, 1, KodNiezrownoleglony);
+            Console.WriteLine(Mask, 2, TworzenieIUruchamianieWatku);
+            Console.WriteLine(Mask, 3, PrzerywanieDzialaniaWatku);
+            Console.WriteLine(Mask, 4, WstrzymywanieIWznawianieDzialaniaWatku);
+            Console.WriteLine(Mask, 5, WatkiDzialajaceWTle);
+            Console.WriteLine(Mask, 6, ZmianaPriorytetuWatku);
+            Console.WriteLine(Mask, 7, ProblemyZGenerowaniemLiczbPseudolosowych);
+            Console.WriteLine(Mask, 8, OsobnyGeneratorLiczbLosowychNaWatek);
+            Console.WriteLine(Mask, 9, CzekanieNaUkonczeniePracyWatkuMetodaJoin);
+            Console.WriteLine(Mask, 10, SekcjeKrytyczne);
+            Console.WriteLine(Mask, 11, PrzesylanieDanychDoWatku);
+            Console.WriteLine(Mask, 12, PulaWatkow);
+            Console.WriteLine(Mask, 13, SygnalizacjaZakonczeniaPracyWatku);
+            Console.WriteLine(Mask, 14, OperacjeAtomowe);
+            Console.WriteLine(Mask, 15, SystemThreadingTimers);
+            Console.WriteLine(Mask, 16, SystemTimersTimer);
+            Console.WriteLine(Mask, 17, Abort);
+            Console.WriteLine("Enter: wyjście");
+        }
+
+        private static void TryStart()
+        {
+            ListingBase listing = null;
 
             switch (_listeningNumber)
             {
-                case 1: listing = new Listing2_1_Kod_niezrównoleglony(); 
+                case 1: listing = new Listing2_1_Kod_niezrównoleglony();
                     break;
                 case 2: listing = new Listing2_2_Tworzenie_I_Uruchamianie_Wątku();
                     break;
@@ -47,23 +106,13 @@ namespace Chapter2
                 case 12: listing = new Listing2_12_Pula_wątków();
                     break;
                 case 13: listing = new Listing2_13_Sygnalizacja_zakończenia_pracy_wątku();
-                    break;
-                default: listing = new Listing2_1_Kod_niezrównoleglony();
-                    break;
+                    break;                
             }
 
+            if (listing == null) 
+                return;
             listing.Start();
-
             Console.ReadKey();
-        }
-
-        private static void GetListeningNumber()
-        {
-            Console.Write("Podaj nr listeningu: ");
-            var line = Console.ReadLine();
-
-            if (!int.TryParse(line, out _listeningNumber))
-                GetListeningNumber();
         }
     }
 }
